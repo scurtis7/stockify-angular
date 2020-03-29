5099b6d447cc38396ea5a6f1cfb7cd612cd6a094
scurtis7 <scurtis7@gmail.com>
2020-03-29 14:08:46 -0400

    add search page and more styling changes

--- src/webapp/src/app/stock-search/stock-search.component.ts
+++ src/webapp/src/app/stock-search/stock-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Stock } from "../stock";
 
 @Component({
   selector: 'app-stock-search',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-search.component.css']
 })
 export class StockSearchComponent implements OnInit {
+  stocks: Stock[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    // this.stocks = [
+    //   {symbol: 'AAPL', name: 'Apple', type: 'Stock', currency: '$', timezone: 'EST', region: 'USA'},
+    //   {symbol: 'MA', name: 'Mastercard', type: 'Stock', currency: '$', timezone: 'EST', region: 'USA'},
+    //   {symbol: 'MSFT', name: 'Microsoft', type: 'Stock', currency: '$', timezone: 'EST', region: 'USA'},
+    //   {symbol: 'WMT', name: 'Walmart', type: 'Stock', currency: '$', timezone: 'EST', region: 'USA'},
+    //   {symbol: 'ZNGA', name: 'Zynga', type: 'Stock', currency: '$', timezone: 'EST', region: 'USA'},
+    //   {symbol: 'SBUX', name: 'Starbucks', type: 'Stock', currency: '$', timezone: 'EST', region: 'USA'}
+    // ];
+  }
+
+  search(searchValue: string) {
+    console.log(searchValue);
   }
 
 }
