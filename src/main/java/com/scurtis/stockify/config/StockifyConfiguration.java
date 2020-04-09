0afc171029c04d924b351c041e2f67e0d30f6579
scurtis7 <scurtis7@gmail.com>
2020-04-08 20:16:08 -0400

    switch to using webflux

--- src/main/java/com/scurtis/stockify/config/StockifyConfiguration.java
+++ src/main/java/com/scurtis/stockify/config/StockifyConfiguration.java
@@ -3,9 +3,18 @@ package com.scurtis.stockify.config;
 import com.scurtis.stockify.converter.StockifyConverter;
 import com.scurtis.stockify.webservice.AlphaVantageService;
 import com.scurtis.stockify.webservice.FinnHubService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 /**
  * Author: Steve Curtis
@@ -39,5 +48,15 @@ public class StockifyConfiguration {
     public StockifyConverter stockifyConverter() {
         return new StockifyConverter();
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> htmlRouter(@Value("classpath:/public/index.html") Resource html) {
+        return route(GET("/"),
+                request -> ok()
+                        .contentType(MediaType.TEXT_HTML)
+                        .bodyValue(html)
+        );
+    }
+
 
 }
